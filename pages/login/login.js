a863662be85f1ae7ac5752fdbb732a0c872e41cc
Raphael Braga <102394065+RaphaelBragaa@users.noreply.github.com>
2024-05-27 01:05:49 -0300

    feat details

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,29 +1,35 @@
-let email;
-let password;
 
+function Login() {
+  console.log('Login');
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('senha').value;
 
-function Login (){
-    email = document.getElementById('email').value;
-    password = document.getElementById('senha').value;
-    const body = {
-        email,
-        password,
-    }
+  const body = {
+    email,
+    password,
+  };
 
-    const promise = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login/auth/login`,body);
+  const promise = axios.post(
+    `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login`,
+    body
+  );
 
-    promise
-        .then(res => {
-            console.log(res)
-            localStorage.setItem(
-                "trackit",
-                JSON.stringify({ token: res.data.token, userImage: res.data.image })
-            );
-            
-            window.location.href = "../principal/principal.html"
+  promise
+    .then(res => {
+      console.log(res);
+      localStorage.setItem(
+        "musicfy",
+        JSON.stringify({ token: res.data.token, userImage: res.data.image })
+      );
+      window.location.href = "../principal/principal.html";
     })
-        .catch(err => {
-            console.log(err)
-            alert("Dados Incorretos")
-        })
+    .catch(err => {
+      console.error(err);
+      alert("Error: " + err.message);
+      if (err.response && err.response.status === 401) {
+        alert("E-mail ou senha inválidos. Por favor, tente novamente.");
+      } else {
+        alert("Ocorreu um erro durante o login. Por favor, tente novamente mais tarde.");
+      }
+    });
 }
